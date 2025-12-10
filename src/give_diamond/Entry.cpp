40bfd570e634178fcfdc0ddadb7f2c0c607bfc18
chen647208 <3308786104@qq.com>
2025-12-10 22:05:33 +0800

    3 个需求错误 1. 我没说只给予一次，每次进服都给予 2. 我没说要打附魔，不掉落和锁定 3. 我也没说要打Lore

--- src/give_diamond/Entry.cpp
+++ src/give_diamond/Entry.cpp
@@ -2,12 +2,7 @@
 #include <ll/api/event/EventBus.h>
 #include <ll/api/event/player/PlayerJoinEvent.h>
 #include <ll/api/mod/RegisterHelper.h>
-#include <mc/nbt/CompoundTagVariant.h>
 #include <mc/safety/RedactableString.h>
-#include <mc/world/actor/player/Inventory.h>
-#include <mc/world/actor/player/PlayerInventory.h>
-#include <mc/world/item/ItemLockHelper.h>
-#include <mc/world/item/ItemLockMode.h>
 #include <mc/world/item/ItemStack.h>
 #include <mc/world/item/VanillaItemNames.h>
 
@@ -23,35 +18,13 @@ bool Entry::load() { return true; }
 bool Entry::enable() {
     mListener = ll::event::EventBus::getInstance().emplaceListener<ll::event::PlayerJoinEvent>(
         [](ll::event::PlayerJoinEvent& event) {
-            auto& inventory = event.self().mInventory->mInventory;
+            auto item = ItemStack(VanillaItemNames::Diamond());
 
-            auto has_diamond = std::find_if(
-                inventory->mItems->begin(),
-                inventory->mItems->end(),
-                [](ItemStack& item) {
-                    return item.getTypeName() == VanillaItemNames::Diamond().getString();
-                }
-            ) != inventory->mItems->end();
+            auto name = Bedrock::Safety::RedactableString();
+            name.set("新手礼包");
+            item.setCustomName(name);
 
-            if (!has_diamond) {
-                auto item = ItemStack(VanillaItemNames::Diamond());
-
-                item.mUserData = std::make_unique<CompoundTag>(CompoundTag{
-                    {"ench",                    ListTag{}                                        },
-                    {"minecraft:keep_on_death", true                                             },
-                    {"minecraft:item_lock",     static_cast<uchar>(ItemLockMode::LockInInventory)}
-                });
-
-                item.setCustomLore({
-                    "§r§6The diamond given by the system | 由系统给予的钻石§r"
-                });
-
-                auto name = Bedrock::Safety::RedactableString();
-                name.set("§r§b新手礼包§r");
-                item.setCustomName(name);
-
-                event.self().addAndRefresh(item);
-            }
+            event.self().addAndRefresh(item);
         }
     );
     return true;
